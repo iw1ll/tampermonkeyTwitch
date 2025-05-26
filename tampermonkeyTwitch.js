c7ebf7f4ac36810299579fe51ef68269163b54e8
iWill <alxs1dd93@gmail.com>
2025-05-26 23:52:55 +0700

    tampermonkey twitch init

---
+++ tampermonkeyTwitch.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         Hide Twitch Element
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  Скрывает элемент с !important стилями
+// @match        https://www.twitch.tv/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log('Twitch Hide Element Started');
+
+    const targetSelector = '.Layout-sc-1xcs6mc-0 .dZwwnJ';
+
+    GM_addStyle(`
+        ${targetSelector} {
+            display: none !important;
+            visibility: hidden !important;
+        }
+    `);
+
+    const observer = new MutationObserver(() => {
+        document.querySelectorAll(targetSelector).forEach(element => {
+            if(getComputedStyle(element).display !== 'none') {
+                element.style.setProperty('display', 'none', 'important');
+                element.style.setProperty('visibility', 'hidden', 'important');
+            }
+        });
+    });
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+        attributes: true,
+        attributeFilter: ['style', 'class']
+    });
+
+    document.querySelectorAll(targetSelector).forEach(element => {
+        element.style.setProperty('display', 'none', 'important');
+    });
+})();
